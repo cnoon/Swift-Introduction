477806ae0e5f0bcb2a88db306ee4fbbe7b457cce
Noon, Christian <christian.noon@nike.com>
2014-08-29 13:31:24 -0700

    Created a playground for subscripts.

---
+++ 12 - Subscripts.playground/section-1.swift
@@ -0,0 +1,51 @@
+
+//=======================================
+//          Subscript Syntax
+//=======================================
+
+// Read-only subscript
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+threeTimesTable[6]
+threeTimesTable[2]
+
+// Multi-parameter subscript structure
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = [Double](count: rows * columns, repeatedValue: 0.0)
+    }
+    
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+matrix
+matrix[1, 0] = 3.2
+matrix
+
+//let someValue = matrix[2, 2] // triggers assert b/c [2, 2] is outside matrix bounds
